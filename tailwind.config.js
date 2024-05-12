2df4320627ea4f1a58fcb09f86029b0ed08be9f5
Ell <78617707+ellharman@users.noreply.github.com>
2024-05-12 15:14:00 +0100

    Fix includes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/*.html", "./.eleventy.js"],
+  content: ["./src/**/*.html", "./.eleventy.js"],
   theme: {
     extend: {},
   },
